b469020c0dd5549721807a8f60ad2ff7bb84e9de
saltKosher <saltswapfinance@gmail.com>
2021-02-18 10:23:26 +1100

    Initial commit

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,85 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://www.goosedefi.com/'
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap'
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool'
+      }
+    ]
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'https://www.goosedefi.com/farms'
+  },
+  {
+    label: 'Nests',
+    icon: 'PoolIcon',
+    href: 'https://www.goosedefi.com/nests'
+  },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: 'https://www.goosedefi.com/lottery'
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+      },
+      {
+        label: 'CoinGecko',
+        href: 'https://www.coingecko.com/en/coins/goose-finance',
+      },
+      {
+        label: 'CoinMarketCap',
+        href: 'https://coinmarketcap.com/currencies/goose-finance/',
+      },
+      {
+        label: 'AstroTools',
+        href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
+      },
+    ]
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      // {
+      //   label: 'Voting',
+      //   href: 'https://voting.pancakeswap.finance',
+      // },
+      {
+        label: "Github",
+        href: "https://github.com/goosedefi/",
+      },
+      {
+        label: "Docs",
+        href: "https://goosedefi.gitbook.io/goose-finance/",
+      },
+      {
+        label: "Blog",
+        href: "https://goosefinance.medium.com/",
+      },
+    ],
+  }
+]
+
+export default config
