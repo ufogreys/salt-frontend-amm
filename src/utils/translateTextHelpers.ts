b3b7009b5e65fa41475d5b3016de2358c8d50586
saltKosher <saltswapfinance@gmail.com>
2021-02-18 16:49:20 +1100

    fix: Use translation fallback if not found

--- src/utils/translateTextHelpers.ts
+++ src/utils/translateTextHelpers.ts
@@ -26,7 +26,7 @@ export const getTranslation = (translations: Array<any>, translationId: number, 
 
 export const TranslateString = (translationId: number, fallback: string) => {
   const { translations } = useContext(TranslationsContext)
-  if (translations[0] === 'error') {
+  if (!translations || !translations.length || translations[0] === 'error') {
     return fallback
   }
   if (translations.length > 0) {
